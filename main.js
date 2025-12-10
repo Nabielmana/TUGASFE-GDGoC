f2e1688a9a4013dfd91c89f74cf2c0fe8ac487a1
Nabiel Ilmana Al-Ghifary <nabielilmana@gmail.com>
2025-12-10 15:45:18 +0700

    feat: money logic

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ const nama = input()
 const umur = Number(input())
 
 if (umur >= 21) {
-    // console.log("Uang lu ada berapa?")
-    // const uang = Number(input())
-    // if (uang < 500000)
-        
+    console.log("Uang lu ada berapa?")
+    const uang = Number(input())
+    if (uang < 500000) {
+        console.log(`Maaf ${nama}, uang kamu cuma ${uang/1000} ribu, datang lagi lain kali`)
+    } else
+        console.log(`Selamat datang ${nama}`)
 } else
     console.log("Maaf " + nama + ", umur kamu kurang", 21 - umur + " tahun lagi")
