dfa51f88404a452306dc51515828120125748943
Nabiel Ilmana Al-Ghifary <nabielilmana@gmail.com>
2025-12-10 15:38:23 +0700

    feat: age logic

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+const input = require('prompt-sync')({sigint: true})
+
+const nama = input()
+const umur = Number(input())
+
+if (umur >= 21) {
+    // console.log("Uang lu ada berapa?")
+    // const uang = Number(input())
+    // if (uang < 500000)
+        
+} else
+    console.log("Maaf " + nama + ", umur kamu kurang", 21 - umur + " tahun lagi")
